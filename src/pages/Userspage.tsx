dd2db8423917f6b2c9673ab3c2579c2d362fffff
Jatin Devaiya <jatin@redefinesolutions.com>
2023-08-18 12:28:53 +0530

    add all product with fack API in home Page

--- src/pages/Userspage.tsx
+++ src/pages/Userspage.tsx
@@ -1,15 +1,14 @@
-import { Button, Grid, Paper } from "@mui/material";
 import { useGetAllUsersQuery } from "../services/Users";
-
+import React from "react";
 interface product {
-  name: string;
-  email: string;
-  phone: string;
-  website: string;
   id: number;
+  price: number;
+  title: string;
+  images: any;
+  category: { name: string; image: string };
 }
 const UsersPage = () => {
-  console.log("aaa", useGetAllUsersQuery());
+  console.log("allproduct", useGetAllUsersQuery());
   const { data, isError, isLoading } = useGetAllUsersQuery();
   if (isLoading) {
     return <p>isLoading...</p>;
@@ -19,18 +18,44 @@ const UsersPage = () => {
   }
 
   return (
-    <Grid spacing={0} container>
-      {data.map((user: product) => (
-        <Grid sm={3}>
-          <Paper sx={{ padding: 2 }}>
-            <h4>Name: {user.name}</h4>
-            <h4> Email:{user.email}</h4>
-            <h4> Phone:{user.phone}</h4>
-            <h4> Website:{user.website}</h4>
-          </Paper>
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <section style={{ background: "#eee" }}>
+        <div className="container">
+          <h3>All Products </h3>
+          <div className="row">
+            {data.map((allProduct: product, index: number) => {
+              return (
+                <>
+                  <div className="col-md-3">
+                    <div className="my-3">
+                      <div
+                        className="card w-100 p-3 bg-image hover-zoom ripple ripple-surface ripple-surface-light"
+                        style={{ height: "400px", width: "400px" }}
+                      >
+                        <img
+                          src={allProduct.images[0]}
+                          className="card-img-top"
+                          alt="Hollywood Sign on The Hill"
+                          height={200}
+                          width={200}
+                        />
+                        <div className="card-body">
+                          <h4 className="card-title">{allProduct.title}</h4>
+                          <div className="d-flex flex-row align-items-center mb-1">
+                            <h4 className="mb-1 me-1">${allProduct.price}</h4>
+                          </div>
+                          <h6>category : {allProduct.category.name}</h6>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
